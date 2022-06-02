adde719109a2efc3e1c18042352458c4a1306789
ota-fayz <ota_2411@mail.ru>
2022-06-02 15:15:26 +0500

    fix photos

--- js/placer.js
+++ js/placer.js
@@ -9,7 +9,7 @@ function init() {
         });
 
     myMap.geoObjects
-        .add(new ymaps.Placemark([42.034611, -87.942657], {
+        .add(new ymaps.Placemark([42.066100, -87.936691], {
         }, {
             preset: 'islands#redDotIconWithCaption'
         }))
